ad017d4ffe3113884ad8979ac87bf71019c3f03a
Scott A. Corbitt <scott.a.corbitt@gmail.com>
2017-10-04 14:48:15 -0600

    fixed js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-$("searchButton").keyup(function(){
+$("searchButton").click(function(){
   var myurl= "https://api.lyrics.ovh/v1/" + $("#artist").val() + "/" + $("#title").val() + "/";
   $.ajax({
     url : myurl,
-    crossDomain: true,
     dataType : "jsonp",
     success : function(parsed_json) {
       console.log(parsed_json);
       $("#lyrics").html(JSON.stringify(parsed_json["lyrics"]));
+    }
+    error : function(error, parsed_json) {
+      console.log(error);
+      console.log(parsed_json);
     }
   });
 });
