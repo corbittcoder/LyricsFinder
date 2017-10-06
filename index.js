f03682cef121b2c63ba02ee4ebf335b5c1721a21
Scott A. Corbitt <scott.a.corbitt@gmail.com>
2017-10-05 21:17:58 -0600

    This is so broken, why doesn't this work at all

--- index.js
+++ index.js
@@ -5,11 +5,24 @@ $("searchButton").click(function(){
     url : myurl,
     dataType : "xml",
     "text xml" : JQuery.parseXML,
-    success : function(xmlDoc) {
-      $xml = $( xmlDoc );
-      $lyrics = $xml.find ("lyrics");
+    success : function(parse) {
+      $lyrics = $parse.find ("lyrics");
       console.log(lyrics);
       $("#lyrics").html( $lyrics.text() );
     }
   });
 });
+$("#searchButton").ready(function() {
+        $.ajax({ type: "POST",
+                        url: myurl,
+                        data: "<formData client=\"\" year=\"\" categories=\"\" tags=\"\" freeText=\"\" count=\"34\" page=\"1\"></formData>",
+                        contentType: "text/xml",
+                        dataType: "xml",
+                        cache: false,
+                        error: function() { alert("No data found."); },
+                        success: function(xml) {
+                            alert("it works");
+                            alert($(xml).find("project")[0].attr("id"));
+                        }
+        });
+    });
